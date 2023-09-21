41c7dedaf335a32ed290fed0351b3a7ed9a239d2
root <root@qq.com>
2023-09-21 15:24:33 +0800

    第二次提交

--- src/main/java/com/atguigu/git/GitTest/Test1.java
+++ src/main/java/com/atguigu/git/GitTest/Test1.java
@@ -3,5 +3,7 @@ package com.atguigu.git.GitTest;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        System.out.println("第二次提交");
+
     }
 }

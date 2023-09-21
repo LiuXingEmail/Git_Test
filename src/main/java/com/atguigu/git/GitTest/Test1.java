d057e0fd158c6885ec79c5b9240d64aabe8a0939
root <root@qq.com>
2023-09-21 15:32:06 +0800

    第六次提交

--- src/main/java/com/atguigu/git/GitTest/Test1.java
+++ src/main/java/com/atguigu/git/GitTest/Test1.java
@@ -6,6 +6,7 @@ public class Test1 {
         System.out.println("第二次提交");
         System.out.println("第三次提交");
         System.out.println("第四次提交");
+        System.out.println("第六次提交");
 
     }
 }

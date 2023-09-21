a51aef758de18582625b07eaf97b77f68e945aa3
root <root@qq.com>
2023-09-21 17:07:19 +0800

    第11次提交

--- src/main/java/com/atguigu/git/GitTest/Test1.java
+++ src/main/java/com/atguigu/git/GitTest/Test1.java
@@ -14,6 +14,7 @@ public class Test1 {
         System.out.println("第8次提交");
         System.out.println("第8次提交");
         System.out.println("第8次提交");
+        System.out.println("push test");
 
     }
 }
